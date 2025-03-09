fee946a5a6a384d6d688855a5f3187f50cac99b0
borisovdim <paragvay85@gmail.com>
2025-03-09 19:21:55 +0200

    added frame tests

---
+++ tests/page-objects/frame.page.ts
@@ -0,0 +1,39 @@
+import { Page } from '@playwright/test';
+import { BasePage } from './base.page';
+
+export class NestedFramePage {
+  page: Page;
+  basePage: BasePage;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.basePage = new BasePage(page);
+  }
+
+  async open() {
+    await this.basePage.goto('nested_frames');
+  }
+
+  frameLeft = () => this.page.frame('frame_left');
+  frameMiddle = () => this.page.frame('frame-middle');
+  frameRight = () => this.page.frame('frame-right');
+  frameBottom = () => this.page.frame('frame-bottom');
+}
+
+export class IFramePage {
+  page: Page;
+  basePage: BasePage;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.basePage = new BasePage(page);
+  }
+
+  async open() {
+    await this.basePage.goto('iframe');
+  }
+
+  closeAlertButton = () => this.page.locator('[role="alert"] button');
+  iFrame = () => this.page.frameLocator('.tox-edit-area__iframe');
+  contentArea = () => this.iFrame().locator('#tinymce');
+}
