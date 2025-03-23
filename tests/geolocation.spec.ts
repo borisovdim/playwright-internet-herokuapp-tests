69facd6b0afdbea9e34f1b81b7474371abc5b4a1
borisovdim <paragvay85@gmail.com>
2025-03-23 18:13:20 +0200

    added test for geolocation

---
+++ tests/geolocation.spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+import { GeolocationPage } from './page-objects/geolocation.page';
+
+test.describe.only('Geolocation', () => {
+  let geolocationPage: GeolocationPage;
+
+  test.beforeEach(async ({ page }) => {
+    geolocationPage = new GeolocationPage(page);
+    await geolocationPage.open();
+    await geolocationPage.getGeoloccation();
+  });
+
+  const longitude = 27.5740700583153;
+  const latitude = 53.89875588242871;
+
+  test.use({
+    geolocation: { longitude: longitude, latitude: latitude },
+    permissions: ['geolocation'],
+  });
+
+  test('Check geolocation', async () => {
+    expect(await geolocationPage.latitude()).toBe(`${latitude}`);
+    expect(await geolocationPage.longitude()).toBe(`${longitude}`);
+  });
+
+  test('Open on Google Maps', async ({ page }) => {
+    await geolocationPage.openOnGoogleMaps();
+    await page.waitForURL(`https://www.google.com/maps?q=${latitude},${longitude}`, { timeout: 10000 });
+  });
+});
---
+++ tests/page-objects/geolocation.page.ts
@@ -0,0 +1,34 @@
+import { Page } from '@playwright/test';
+import { BasePage } from './base.page';
+
+export class GeolocationPage {
+  page: Page;
+  basePage: BasePage;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.basePage = new BasePage(page);
+  }
+
+  getGeolocationButton = () => this.page.getByRole('button', { name: 'Where am I?' });
+  latitude = () => this.page.locator('#lat-value').textContent();
+  longitude = () => this.page.locator('#long-value').textContent();
+  mapLink = () => this.page.locator('#map-link a');
+  linkValue = () => this.mapLink().getByRole('link').getAttribute('href');
+  rejectGoogleButton = () => this.page.getByRole('button', { name: 'Reject All' });
+
+  async open() {
+    this.basePage.goto('geolocation');
+  }
+
+  async getGeoloccation() {
+    await this.getGeolocationButton().click();
+  }
+
+  async openOnGoogleMaps() {
+    await this.mapLink().click();
+    if (await this.rejectGoogleButton().isVisible({ timeout: 5000 })) {
+      await this.rejectGoogleButton().click();
+    }
+  }
+}
